8132a23c9ec6500d5d984139b844803f791d8d02
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-25 10:17:43 +0100

    Add a mars rover application stub

--- src/tests/test_mars_rover.py
+++ src/tests/test_mars_rover.py
@@ -1,4 +1,14 @@
-class TestMarsRover:
+from typing import Optional
 
-    def test_fails(self) -> None:
-        assert 0
+
+class MarsRoverApplication:
+
+    def rover_position(self) -> Optional[str]:
+        return None
+
+
+class TestMarsRoverApplication:
+
+    def test_reports_no_rover_position_before_the_rover_landed(self) -> None:
+        app = MarsRoverApplication()
+        assert app.rover_position() is None
