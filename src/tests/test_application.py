7647fed4e70db89a0444fb48a0c72c35058d1f34
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-25 15:22:02 +0100

    Separate application tests from domain tests

--- src/tests/test_application.py
+++ src/tests/test_application.py
@@ -2,8 +2,6 @@ import pytest
 
 from mars_rover.application import MarsRoverApplication
 from mars_rover.application import UserInputError
-from mars_rover.coordinates import Coordinates
-from mars_rover.direction import Direction
 
 
 class TestMarsRoverApplication:
@@ -121,40 +119,3 @@ class TestMarsRoverApplication:
         with pytest.raises(UserInputError) as error:
             app.execute('x')
         assert str(error.value) == "Unknown command: 'x'"
-
-
-class TestCoordinates:
-
-    def test_two_equal_coordinates(self) -> None:
-        assert Coordinates(3, 4) == Coordinates(3, 4)
-
-    def test_two_coordinates_with_different_horizontal_point(self) -> None:
-        assert Coordinates(3, 4) != Coordinates(2, 4)
-
-    def test_two_coordinates_with_different_vertical_point(self) -> None:
-        assert Coordinates(3, 4) != Coordinates(3, 5)
-
-
-class TestDirection:
-
-    @pytest.mark.parametrize(
-        ('symbol', 'direction'), [
-            ('N', Direction.north()),
-            ('S', Direction.south()),
-            ('E', Direction.east()),
-            ('W', Direction.west()),
-        ],
-    )
-    def test_can_be_created_from_symbol(self, symbol: str, direction: Direction) -> None:
-        assert Direction.for_symbol(symbol) == direction
-
-    def test_can_not_be_created_from_unknown_symbol(self) -> None:
-        with pytest.raises(ValueError) as error:
-            Direction.for_symbol('X')
-        assert str(error.value) == 'Unknown direction: X'
-
-    def test_two_equal_directions(self) -> None:
-        assert Direction.north() == Direction.north()
-
-    def test_two_different_directions(self) -> None:
-        assert Direction.north() != Direction.south()
