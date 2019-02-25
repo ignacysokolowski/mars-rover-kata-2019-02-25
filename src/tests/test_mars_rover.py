6c5169c032187f39b58da39e12e9b3ef16ed05fb
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-25 10:20:05 +0100

    Add landing a rover with the given position

--- src/tests/test_mars_rover.py
+++ src/tests/test_mars_rover.py
@@ -3,8 +3,14 @@ from typing import Optional
 
 class MarsRoverApplication:
 
+    def __init__(self) -> None:
+        self._rover_position: Optional[str] = None
+
     def rover_position(self) -> Optional[str]:
-        return None
+        return self._rover_position
+
+    def land_rover(self, position: str) -> None:
+        self._rover_position = position
 
 
 class TestMarsRoverApplication:
@@ -12,3 +18,8 @@ class TestMarsRoverApplication:
     def test_reports_no_rover_position_before_the_rover_landed(self) -> None:
         app = MarsRoverApplication()
         assert app.rover_position() is None
+
+    def test_lands_rover_with_the_given_position(self) -> None:
+        app = MarsRoverApplication()
+        app.land_rover('3 4')
+        assert app.rover_position() == '3 4'
